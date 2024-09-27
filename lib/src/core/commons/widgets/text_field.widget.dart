724f89fec55165d84f9b78debff415507936dfa7
GUSTI KRISNA PRANATA <140774014+gstkrsnaprnta@users.noreply.github.com>
2024-09-27 22:39:35 +0800

    feat: add optional suffixIcon to CustomTextField for better customization

--- lib/src/core/commons/widgets/text_field.widget.dart
+++ lib/src/core/commons/widgets/text_field.widget.dart
@@ -1,14 +1,14 @@
-import 'package:aplikasi_point_of_sale/src/core/commons/widgets/space_widgets.dart';
+import 'package:aplikasi_point_of_sale/src/core/commons/widgets/space_widget.dart';
 import 'package:flutter/material.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   final TextEditingController controller;
   final String label;
   final Function(String value)? onChanged;
   final bool obscureText;
   final TextInputType? keyboardType;
   final bool showLabel;
-  final Widget? suffixIcon;
+  final Widget? suffixIcon; // Suffix icon dijadikan opsional
 
   const CustomTextField({
     super.key,
@@ -18,17 +18,30 @@ class CustomTextField extends StatelessWidget {
     this.obscureText = false,
     this.keyboardType,
     this.showLabel = true,
-    this.suffixIcon,
+    this.suffixIcon, // Tidak required
   });
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool _isObscured = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _isObscured = widget.obscureText;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        if (showLabel) ...[
+        if (widget.showLabel) ...[
           Text(
-            label,
+            widget.label,
             style: const TextStyle(
               fontSize: 14,
               fontWeight: FontWeight.w700,
@@ -37,12 +50,26 @@ class CustomTextField extends StatelessWidget {
           const SpaceHeight(12.0),
         ],
         TextFormField(
-          controller: controller,
-          onChanged: onChanged,
-          obscureText: obscureText,
-          keyboardType: keyboardType,
+          controller: widget.controller,
+          onChanged: widget.onChanged,
+          obscureText: _isObscured,
+          keyboardType: widget.keyboardType,
           decoration: InputDecoration(
-            suffixIcon: suffixIcon,
+            // Gunakan suffixIcon jika diberikan, jika tidak dan obscureText = true, gunakan IconButton sebagai default.
+            suffixIcon: widget.suffixIcon ??
+                (widget.obscureText
+                    ? IconButton(
+                        icon: Icon(
+                          _isObscured ? Icons.visibility_off : Icons.visibility,
+                          color: Colors.grey,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _isObscured = !_isObscured;
+                          });
+                        },
+                      )
+                    : null),
             border: OutlineInputBorder(
               borderRadius: BorderRadius.circular(16.0),
               borderSide: const BorderSide(color: Colors.grey),
@@ -51,7 +78,7 @@ class CustomTextField extends StatelessWidget {
               borderRadius: BorderRadius.circular(16.0),
               borderSide: const BorderSide(color: Colors.grey),
             ),
-            hintText: label,
+            hintText: widget.label,
           ),
         ),
       ],
